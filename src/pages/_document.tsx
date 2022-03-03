92fbaa192e0aa2aed8d78b180f25f67eff880427
Pedro Paulo <pedropaulotj@gmail.com>
2022-03-03 19:25:49 -0300

    fix: Ajuste no theme-color do navegador para mobile

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
         <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
-        <meta name="theme-color" content="#131313"/>
+        <meta name="theme-color" content="#181B23"/>
         </Head>
         <body>
           <Main />
