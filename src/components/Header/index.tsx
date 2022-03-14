eb0adab241ab03ac1327bcbbb3c969313276cb23
Pedro Paulo <pedropaulotj@gmail.com>
2022-03-14 16:40:27 -0300

    fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,11 @@
-import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { RiMenuLine } from "react-icons/ri";
-import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 
 export function Header() {
   const { onOpen } = useSidebarDrawer();
