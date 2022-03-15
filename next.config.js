7346628a741ccf7b1052cf0068cbb401cda2bbd3
Pedro Paulo <pedropaulotj@gmail.com>
2022-03-15 16:50:47 -0300

    fix: CORS error on Vercel

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
 
 // next.config.js
 module.exports = {
+  reactStrictMode: true,
   async headers() {
     return [
       {
